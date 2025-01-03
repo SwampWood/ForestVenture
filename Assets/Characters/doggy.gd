c9e2a0cbce3089abd3b06ad46fb1035620f44529
SwampWood <danya.ney@yandex.ru>
2025-01-04 02:05:23 +0300

    Added zero-room
    Concept for spirit - entity tied to character selection in the start of the run. Added texture for campfire. Resolved doggy not following tamer correctly bug.

--- Assets/Characters/doggy.gd
+++ Assets/Characters/doggy.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 280
 const JUMP_VELOCITY = -400.0
 @onready var player = $"../../Allies/Tamer_male"
+@onready var anim = get_node("AnimatedSprite2D")
 var idle = false
 
 func _physics_process(delta: float) -> void:
@@ -12,16 +13,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		
 	var direction = (player.position - self.position).normalized()
-	if idle == false:
-		velocity.x = direction.x * SPEED
+	if idle:
+		velocity.x -= direction.x * 10
+		if abs(velocity.x) <= 20:
+			velocity.x = 0
+		
+		anim.play("Idle")
 	else:
-		velocity.x = 0
+		velocity.x = direction.x * SPEED
+		anim.play("Run")
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Tamer_male":
 		idle = true
+		anim.play("Run")
 
 
 func _on_area_2d_body_shape_exited(_body_rid, body, _body_shape_index, _local_shape_index):
--- Assets/Characters/druid_male.gd
+++ Assets/Characters/druid_male.gd
@@ -17,6 +17,9 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_pressed("next_level"):
+		get_tree().change_scene_to_file("res://Assets/Maps/room_0.tscn")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
